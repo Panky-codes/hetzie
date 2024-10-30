3a9b7b93b2edfc9fc8e879871912e850d1cd4c46
Pankaj Raghav <p.raghav@samsung.com>
2024-10-30 11:09:24 +0530

    age: remove all secrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,7 +1,6 @@
 let
-  quentin = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID/6Oh5VLHRAzoeiC53hQzGJ3wkSR4qGR7erqlU7fpWp root@quentin";
+#  host = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID/6Oh5VLHRAzoeiC53hQzGJ3wkSR4qGR7erqlU7fpWp root@quentin";
 in
 {
-  "sshhost_priv.age".publicKeys = [ quentin ];
-  "sshhost_pub.age".publicKeys = [ quentin ];
+# "sshhost_priv.age".publicKeys = [ host ];
 }
