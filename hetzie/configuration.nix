a50e5b921384d11034cd81baee180c6b9cd83bfa
Pankaj Raghav <p.raghav@samsung.com>
2024-10-30 14:31:51 +0530

    Add andreas

--- hetzie/configuration.nix
+++ hetzie/configuration.nix
@@ -77,6 +77,21 @@
     hashedPassword = "$y$j9T$6ZmrGfiJ4YiqKG19C9cOh1$wSFZkYz1AyW/wNlaUcfphh/W9RUCvECJminXOmtI0xC";
   };
 
+  users.users.andreas = {
+    isNormalUser = true;
+    home = "/home/andreas";
+    description = "Andreas";
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+      "libvirtd"
+    ];
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDoXHJ/4j7kIHmMtLMmqSusvcJpTYUsRp8mZM6QV3HE4 aeh@aeh-pocket"
+    ];
+    hashedPassword = "$y$j9T$6ZmrGfiJ4YiqKG19C9cOh1$wSFZkYz1AyW/wNlaUcfphh/W9RUCvECJminXOmtI0xC";
+  };
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
