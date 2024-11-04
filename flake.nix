6eaf9d8e526939d31c6e8659e66a66a948f73e34
Pankaj Raghav <p.raghav@samsung.com>
2024-11-04 22:32:41 +0530

    home-manager: remove all references to home manager

--- flake.nix
+++ flake.nix
@@ -7,19 +7,12 @@
     agenix.url = "github:ryantm/agenix";
     disko.url = "github:nix-community/disko";
     disko.inputs.nixpkgs.follows = "nixpkgs";
-    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
-    home-manager = {
-      url = "github:nix-community/home-manager/release-24.05";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs =
     inputs@{
       nixpkgs,
-      home-manager,
       agenix,
-      nixos-hardware,
       disko,
       ...
     }:
@@ -38,14 +31,6 @@
             ./hetzie/age.nix
             ./hetzie/users.nix
             { environment.systemPackages = [ agenix.packages.x86_64-linux.default ]; }
-            home-manager.nixosModules.home-manager
-            {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-
-              home-manager.users.panky = import ./hetzie/panky-home.nix;
-            }
-
           ];
         };
       };
