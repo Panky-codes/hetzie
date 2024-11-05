fddf97a10e548c6fdcc837e4cb7cddb880845d07
Pankaj Raghav <p.raghav@samsung.com>
2024-11-05 17:54:27 +0530

    Add Daniel

--- hetzie/users.nix
+++ hetzie/users.nix
@@ -31,4 +31,19 @@
     ];
     hashedPassword = "$y$j9T$6ZmrGfiJ4YiqKG19C9cOh1$wSFZkYz1AyW/wNlaUcfphh/W9RUCvECJminXOmtI0xC";
   };
+
+  users.users.dagomez = {
+    isNormalUser = true;
+    home = "/home/dagomez";
+    description = "Daniel";
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+      "libvirtd"
+    ];
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMImV6kLnytXQt6l1alTWOnOuu9+fvkTyjOyReVNcUx1 Daniel Gomez"
+    ];
+    hashedPassword = "$y$j9T$6ZmrGfiJ4YiqKG19C9cOh1$wSFZkYz1AyW/wNlaUcfphh/W9RUCvECJminXOmtI0xC";
+  };
 }
